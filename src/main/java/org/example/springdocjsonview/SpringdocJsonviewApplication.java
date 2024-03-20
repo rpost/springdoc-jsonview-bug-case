1fcc972a27cd2109776072ff5bb21bac9d502312
Radoslaw Postolowicz <radoslaw.postolowicz@cronn.de>
2024-03-20 12:25:17 +0100

    Create spring boot app

---
+++ src/main/java/org/example/springdocjsonview/SpringdocJsonviewApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdocjsonview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocJsonviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocJsonviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springdocjsonview/SpringdocJsonviewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdocjsonview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocJsonviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
